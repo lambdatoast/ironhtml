6ce547a27d399764f0dc1568bb7f54fa33fdf3e8
Alexander Noriega <lambdatoast@gmail.com>
2013-11-14 00:26:22 -0300

    Add pertinent WHATWG link to the README.

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ IronHTML is a minimal HTML library for Scala with a functional approach.
 
 The following material is being used as a guideline for the design of constraints and correctness of HTML structures.
 
-* [HTML5: A vocabulary and associated APIs for HTML and XHTML](http://www.w3.org/html/wg/drafts/html/CR/):
-* [Content models](http://www.w3.org/html/wg/drafts/html/CR/dom.html#content-models)
+* (W3C) [HTML5: A vocabulary and associated APIs for HTML and XHTML](http://www.w3.org/html/wg/drafts/html/CR/)
+* (W3C) [Content models](http://www.w3.org/html/wg/drafts/html/CR/dom.html#content-models)
+* (WHATWG) [Elements](http://www.whatwg.org/specs/web-apps/current-work/multipage/elements.html)
--- src/main/scala/ironhtml.scala
+++ src/main/scala/ironhtml.scala
@@ -70,24 +70,35 @@ object IronHTML {
       case HTMLContent(xs) => xs.foldLeft("") { (acc, e) => acc + render(e) }
       case Text(c) => c
     }
+    implicit class SyntaxRender(e: HTMLExpr) {
+      def render: String = renderer.render(e)
+    }
   }
-   
 
-  import ops._
-  import constructors._
-  import renderer._
+  def main(args: Array[String]) {
+    import ops._
+    import constructors._
+    import renderer._
 
-  val e1 = a("111".text).copy(attrs = List("href" -> "http://www.google.com"))
-  val e2 = strong("222".text)
-  val e3 = span("333".text)
-  val e4 = "input".void.copy(attrs = List("type" -> "checkbox", "checked" -> ""))
-  val e5 = fold(List(e1, e2, e3, e4).map(li))(ul(empty))(add)
-  val e6 = map(e5) { 
-    case HTMLElement("ul", c, _) => p(c)
-    case HTMLElement("li", c, _) => div(c)
-    case e => e 
+    val e1 = a("111".text).copy(attrs = List("href" -> "http://www.google.com"))
+    val e2 = strong("222".text)
+    val e3 = span("333".text)
+    val e4 = "input".void.copy(attrs = List("type" -> "checkbox", "checked" -> ""))
+    val e5 = fold(List(e1, e2, e3, e4).map(li))(ul(empty))(add)
+    val e6 = map(e5) { 
+      case HTMLElement("ul", c, _) => p(c)
+      case HTMLElement("li", c, _) => div(c)
+      case e => e 
+    }
+    val t1 = render(e5) // <ul><li><a href="http://www.google.com">111</a></li><li><strong>222</strong></li><li><span>333</span></li><li><input type="checkbox" checked></li></ul>
+    val t2 = render(e6) // <p><div><a href="http://www.google.com">111</a></div><div><strong>222</strong></div><div><span>333</span></div><div><input type="checkbox" checked></div></p>
+    println(t1)
+
+    println(render(li("hey".text) ++ li("second".text)))
+    println(map(e5) {
+      case list@HTMLElement("ul", c, _) => list <<< ul(li("hey".text) ++ li("second".text))
+      case e => e 
+    } render)
   }
-  val t1 = render(e5) // <ul><li><a href="http://www.google.com">111</a></li><li><strong>222</strong></li><li><span>333</span></li><li><input type="checkbox" checked></li></ul>
-  val t2 = render(e6) // <p><div><a href="http://www.google.com">111</a></div><div><strong>222</strong></div><div><span>333</span></div><div><input type="checkbox" checked></div></p>
 
 }
