50ec0e02dcccfb37ccdc214abd87d36a4df33b34
Alexander Noriega <lambdatoast@gmail.com>
2013-11-18 00:41:19 -0300

    Give up on the union-based (both tagged and untagged) attempt to express the HTML spec's content model rules with the type system. Remove all related code and go back to the free-form HTML builder. TODO: design separate validation module.

--- src/main/scala/Element.scala
+++ src/main/scala/Element.scala
@@ -1,62 +1,28 @@
 package ironhtml
 
 object Element {
-  import ContentModels._
-  import Union._
 
-  sealed trait ElementType
-  case object Normal extends ElementType
-  case object Void extends ElementType
+  type HTMLAttribute = (String,String)
+  sealed trait HTMLExpr
+  case class HTMLElement(name: String, content: HTMLExpr, attrs: List[HTMLAttribute] = Nil) extends HTMLExpr
+  case class HTMLVoidElement(name: String, attrs: List[HTMLAttribute] = Nil) extends HTMLExpr
+  case class HTMLContent(content: List[HTMLExpr]) extends HTMLExpr
+  case class Text(content: String) extends HTMLExpr
 
-  type Attribute = (String,String)
+  object constructors {
+    def a(e: HTMLExpr): HTMLElement = "a".el(e)
+    def div(e: HTMLExpr): HTMLElement = "div".el(e)
+    def empty: HTMLExpr = HTMLContent(Nil)
+    def li(e: HTMLExpr): HTMLElement = "li".el(e)
+    def p(e: HTMLExpr): HTMLElement = "p".el(e)
+    def span(e: HTMLExpr): HTMLElement = "span".el(e)
+    def strong(e: HTMLExpr): HTMLElement = "strong".el(e)
+    def ul(e: HTMLExpr): HTMLElement = "ul".el(e)
 
-  sealed trait Element {
-    val name: String
-    val elType: ElementType
-    val attrs: List[Attribute]
-    val content: Option[Either[Element,Text]]
-  }
-  trait AddAnyOf[B[_], R] {
-    type AllowedContent[_] = B[_]
-    def add[T : AllowedContent](e: T): R 
-  }
-  trait Add[T,U] {
-    def add(e: T): U 
-  }
-  case class Anchor(content: Option[Either[Element, Text]], attrs: List[Attribute] = Nil) extends Element with AddAnyOf[`|v|3`[Phrasing,Interactive,Embedded]#λ, Element] {
-    val name = "a"
-    val elType = Normal
-    def add[T : AllowedContent](e: T) = e match {
-      case e: Phrasing => this
-      case p: Interactive => this
-      case p: Embedded => this
+    implicit class SyntaxString(s: String) {
+      def el(content: HTMLExpr) = HTMLElement(s, content)
+      def void = HTMLVoidElement(s)
+      def text = Text(s)
     }
   }
-  case class Select(content: Option[Either[Element, Text]], attrs: List[Attribute] = Nil) extends Element with Flow with Phrasing with Interactive {
-    val elType = Normal
-    val name = "select"
-  }
-  case class Span(content: Option[Either[Element, Text]], attrs: List[Attribute] = Nil) extends Element with Flow with Phrasing with Add[Phrasing, Element] {
-    val elType = Normal
-    val name = "span"
-    def add(e: Phrasing) = this
-  }
-  /**
-   * TODO: Should only add "Flow content, but with no main element descendants."
-   * @see [[http://www.whatwg.org/specs/web-apps/current-work/multipage/sections.html#the-nav-element]]
-   */
-  case class Nav(content: Option[Either[Element, Text]], attrs: List[Attribute] = Nil) extends Element with Flow with Sectioning with Add[Flow, Element] {
-    val elType = Normal
-    val name = "nav"
-    def add(e: Flow) = this
-  }
-
-  sealed trait Content
-  case class Text(text: String) extends Content
-
-  implicit object evidences {
-    implicit def toRight(t: Text): Either[Nothing, Text] = Right(t)
-    implicit def toLeft(e: Element): Either[Element, Nothing] = Left(e)
-  }
-
 }
--- src/main/scala/ironhtml.scala
+++ src/main/scala/ironhtml.scala
@@ -1,15 +1,34 @@
 package ironhtml
+import scala.language.postfixOps
 
 object IronHTML {
-  import Element._
-  import Element.evidences._
-  import Renderer._
 
   def main(args: Array[String]) {
-    val a = Anchor(Some(Text("go to github")), List("href" -> "http://github.com"))
-    println("")
-    println(render(a))
-    println("")
+    import Operations._
+    import Element._
+    import Element.constructors._
+    import Renderer._
+
+    val e1 = a("111".text).copy(attrs = List("href" -> "http://www.google.com"))
+    val e2 = strong("222".text)
+    val e3 = span("333".text)
+    val e4 = "input".void.copy(attrs = List("type" -> "checkbox", "checked" -> ""))
+    val e5 = fold(List(e1, e2, e3, e4).map(li))(ul(empty))(add)
+    val e6 = map(e5) { 
+      case HTMLElement("ul", c, _) => p(c)
+      case HTMLElement("li", c, _) => div(c)
+      case e => e 
+    }
+    val t1 = render(e5) // <ul><li><a href="http://www.google.com">111</a></li><li><strong>222</strong></li><li><span>333</span></li><li><input type="checkbox" checked></li></ul>
+    val t2 = render(e6) // <p><div><a href="http://www.google.com">111</a></div><div><strong>222</strong></div><div><span>333</span></div><div><input type="checkbox" checked></div></p>
+    println(t1)
+
+    println(render(li("hey".text) ++ li("second".text)))
+    println(map(e5) {
+      case list@HTMLElement("ul", c, _) => list <<< ul(li("hey".text) ++ li("second".text))
+      case e => e 
+    } render)
   }
 
 }
+
