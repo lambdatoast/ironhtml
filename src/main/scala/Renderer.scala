eea00c0233930d00d9fb066d584f415b38aea957
Alexander Noriega <lambdatoast@gmail.com>
2013-11-19 22:30:26 -0300

    Bring back the Renderer logic that works for the latest abstraction approach for Element.

--- src/main/scala/Renderer.scala
+++ src/main/scala/Renderer.scala
@@ -2,15 +2,23 @@ package ironhtml
 
 object Renderer {
   import Element._
-  def attrs(xs: List[HTMLAttribute]): String = xs.foldLeft("") { (acc,a) => acc + " " + a._1 + (if (a._2 != "") s"""="${a._2}"""" else "") }
-  def render(e: HTMLExpr): String = e match {
-    case HTMLElement(n,c,xs) => s"<${n}${attrs(xs)}>${render(c)}</$n>"
-    case HTMLVoidElement(n,xs) => s"<${n}${attrs(xs)}>"
-    case HTMLContent(xs) => xs.foldLeft("") { (acc, e) => acc + render(e) }
-    case Text(c) => c
-  }
-  implicit class SyntaxRender(e: HTMLExpr) {
-    def render: String = Renderer.render(e)
-  }
+  def attrs(xs: List[Attribute]): String = xs.foldLeft("") { (acc,a) => acc + " " + a._1 + (if (a._2 != "") s"""="${a._2}"""" else "") }
+
+  def render(e: Element): String =
+    e.content match {
+      case Some(Left(c)) => e.elType match {
+        case Normal => s"<${e.name}${attrs(e.attrs)}>${render(c)}</${e.name}>"
+        case Void   => s"<${e.name}${attrs(e.attrs)}>"
+      }
+      case Some(Right(Text(c))) => e.elType match {
+        case Normal => s"<${e.name}${attrs(e.attrs)}>${c}</${e.name}>"
+        case Void   => s"<${e.name}${attrs(e.attrs)}>"
+      }
+      case None => e.elType match {
+        case Normal => s"<${e.name}${attrs(e.attrs)}></${e.name}>"
+        case Void   => s"<${e.name}${attrs(e.attrs)}>"
+      }
+    }
+
 }
 
