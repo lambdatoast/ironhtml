f84993dbe07bef74afc9c2ac52366eb192201d2f
Alexander Noriega <lambdatoast@gmail.com>
2013-11-20 04:32:31 -0300

    Add another example of usage to the test file.

--- src/main/scala/ironhtml.scala
+++ src/main/scala/ironhtml.scala
@@ -15,17 +15,19 @@ object IronHTML {
     import Operations.evidences._
     import Element.NonElementContent.evidences._
 
-    val a = Div(None, List("class" -> "row-fluid")).add(Div(None))
+    val a = Div(None, List("class" -> "row-fluid"))
     val b = Div(None) |+| H1(Some(Text("Introduction")), List("id" -> "introduction"))
     // val c = H1(None).add(Div(None)) -- Compile Error :)
     val c = Div(Some(a ++ b))
     val d = Div(Some(Text("Hello "))) <<< Div(Some(Text("World!")))
+    val e = Div(None) |+| H1(Some(Text("Conclusion")), List("id" -> "conclusion"))
 
     println("")
     println(render(a))
     println(render(b))
     println(render(c))
     println(render(d))
+    println(render(a |+| b <<< e))
     println("")
   }
 
