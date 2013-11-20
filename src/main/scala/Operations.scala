65c31840d807970aec38dd5bcb0c7cc47478f5a8
Alexander Noriega <lambdatoast@gmail.com>
2013-11-20 04:08:48 -0300

    Clean up commented code.

--- src/main/scala/Operations.scala
+++ src/main/scala/Operations.scala
@@ -25,6 +25,7 @@ object Operations {
 
   trait MergeEvidences {
     import Element.NonElementContent.evidences._
+
     implicit object divMerge extends Merge[Div] {
       def merge[T <: Element](a: Div, b: T) = a.content match {
         case Some(Left(ea)) => b.content match {
@@ -39,41 +40,4 @@ object Operations {
       }
     }
   }
-
-  /*
-  def fold(xs: List[HTMLExpr])(z: HTMLExpr)(f: (HTMLExpr, HTMLExpr) => HTMLExpr): HTMLExpr =
-    xs.foldLeft(z)(f)
-  def map(e: HTMLExpr)(f: HTMLExpr => HTMLExpr): HTMLExpr = e match {
-    case HTMLElement(n,c,xs) => f(HTMLElement(n, map(c)(f), xs))
-    case HTMLVoidElement(n,_) => f(e)
-    case HTMLContent(xs) => HTMLContent(xs.foldRight(List[HTMLExpr]())((e, acc) => map(e)(f) :: acc))
-    case Text(c) => f(e)
-  }
-  def concat(e1: HTMLExpr, e2: HTMLExpr): HTMLExpr =
-    HTMLContent(List(e1, e2))
-
-  def add(e1: HTMLExpr, e2: HTMLExpr): HTMLExpr =
-    e1 match {
-      case HTMLElement(n,c,xs) => HTMLElement(n, concat(c,e2), xs)
-      case _ => concat(e1, e2)
-    }
-
-  def merge(e1: HTMLExpr, e2: HTMLExpr): HTMLExpr =
-    e1 match {
-      case HTMLElement(n,c,xs) => e2 match {
-        case HTMLElement(n2,c2,ys) if (n2 == n) => HTMLElement(n, concat(c, c2), xs ++ ys)
-        case _ => concat(e1,e2)
-      }
-      case _ => concat(e1, e2)
-    }
-
-  implicit class SyntaxHTMLExpr(e: HTMLExpr) {
-    def add(e2: HTMLExpr): HTMLExpr = Operations.add(e, e2)
-    def `|+|`(e2: HTMLExpr): HTMLExpr = Operations.add(e, e2)
-    def concat(e2: HTMLExpr): HTMLExpr = Operations.concat(e, e2)
-    def `++`(e2: HTMLExpr): HTMLExpr = Operations.concat(e, e2)
-    def merge(e2: HTMLExpr): HTMLExpr = Operations.merge(e, e2)
-    def `<<<`(e2: HTMLExpr): HTMLExpr = Operations.merge(e, e2)
-  }
-  */
 }
